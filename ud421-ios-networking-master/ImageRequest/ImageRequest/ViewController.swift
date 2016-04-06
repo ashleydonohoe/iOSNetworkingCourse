3bfeefb5339b2b79b0cc763a7e3ba26aa5718857
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-04-06 03:14:31 -0400

    finished imagerequest app

--- ud421-ios-networking-master/ImageRequest/ImageRequest/ViewController.swift
+++ ud421-ios-networking-master/ImageRequest/ImageRequest/ViewController.swift
@@ -22,5 +22,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // TODO: Add all the networking code here!
+        let imageURL = NSURL(string: "http://kathyharrisbooks.com/wp-content/uploads/2013/05/Misty-Edwards.jpg")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(imageURL) { (data, response, error) in
+            if error == nil {
+                let downloadedImage = UIImage(data: data!)
+                performUIUpdatesOnMain {
+                    self.imageView.image = downloadedImage
+                }
+            }
+        }
+        
+        task.resume()
     }
 }
